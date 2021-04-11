97393f76c123b121d8c415a0c2abe6bd6faf2786
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-04-11 03:00:27 -0300

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -127,14 +127,12 @@ async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     output = (
         "**Bot funcionando normalmente** \n"
-        f"•  🤖 LzinhoBot: \n"
-        f"v{PURPLEBOT_VERSION} \n"
-        f"•  ⚙️ Telethon: \n"
-        f"v{version.__version__} \n"
-        f"•  🐍 Python: \n"
-        f"v{python_version()} \n"
-        f"•  👤 Usuário: \n"
-        f"{DEFAULTUSER} \n"
+        f"•`  🤖 LzinhoBot: \n`"
+        f"** v{PURPLEBOT_VERSION} \n**"
+        f"•`  ⚙️ Telethon: \n`"
+        f"** v{version.__version__} \n**"
+        f"•`  🐍 Python: \n`"
+        f"** v{python_version()} \n**"
 
     )
     if ALIVE_LOGO:
