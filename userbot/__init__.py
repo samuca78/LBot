7d63e2d35a53bb63c821c2b04c88b9f7eba6b1ce
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-04-10 23:48:54 -0300

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -169,7 +169,7 @@ GENIUS = os.environ.get("GENIUS_ACCESS_TOKEN") or None
 USR_TOKEN = os.environ.get("USR_TOKEN_UPTOBOX") or None
 
 # LzinhoBot version
-LZINHOBOT_VERSION = "5.0.1"
+PURPLEBOT_VERSION = "5.0.1"
 
 # 'bot' variable
 if STRING_SESSION:
