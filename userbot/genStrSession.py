66e9b49091d29e6b6e5f1db7696fc6df26312cd7
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-04-11 14:55:59 -0300

    Update genStrSession.py

--- userbot/genStrSession.py
+++ userbot/genStrSession.py
@@ -1,10 +1,9 @@
 # pylint: disable=invalid-name, missing-module-docstring
 #
-# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
 #
-# This file is part of < https://github.com/UsergeTeam/Userge > project,
+# This file is part of < https://github.com/samuca78/LzinhoBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
+# Please see < https://github.com/samuca78/LzinhoBot/blob/master/LICENSE >
 #
 # All rights reserved.
 
@@ -20,14 +19,14 @@ if os.path.isfile("config.env"):
 
 async def genStrSession() -> None:  # pylint: disable=missing-function-docstring
     async with Client(
-            "Userge",
+            "LzinhoBot",
             api_id=int(os.environ.get("API_ID") or input("Enter Telegram APP ID: ")),
             api_hash=os.environ.get("API_HASH") or input("Enter Telegram API HASH: ")
     ) as userge:
         print("\nprocessing...")
-        await userge.send_message(
-            "me", f"#USERGE-X #HU_STRING_SESSION\n\n```{await userge.export_session_string()}```")
-        print("Done !, session string has been sent to saved messages!")
+        await lzinhobot.send_message(
+            "me", f"#LZINHOBOT #STRING_SESSION\n\n```{await lzinhobot.export_session_string()}```")
+        print("Pronto! Sua String Session foi enviada para as mensagens salvas!")
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(genStrSession())
