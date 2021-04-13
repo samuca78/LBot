216f8277cba2fb9eedb36f485386fa6d74db6dcd
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-04-12 21:56:33 -0300

    Update untitled.py

--- userbot/modules/untitled.py
+++ userbot/modules/untitled.py
@@ -1,6 +1,16 @@
-  (
+from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, bot
+from userbot.events import register
+from userbot.utils import time_formatter
+
    @register(outgoing=True, pattern=r"^\.fnix$"*)
    async def fnix(e):
      await e.edit(
        "**tu é um corno fnix** \n"
        )
+
+
+register(outgoing=True, pattern=r"^\.samu$")
+async def samu(e):
+  await e.edir(
+    "samuel é ruim, ruim em python, mds"
+    )
