6e71b4f0e4292c36b20f5a7728ac86af503411ec
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-04-13 01:56:13 -0300

    update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -134,7 +134,7 @@ async def amireallyalive(alive):
         f"•** Telethon                                  v{version.__version__} \n**"
         f"•** Python                                    v{python_version()} \n**"
         "**============================ \n**"
-        [repo](github.com/samuca78/LzinhoBot)
+        ['repo''](github.com/samuca78/LzinhoBot)
     )
     if ALIVE_LOGO:
         try:
