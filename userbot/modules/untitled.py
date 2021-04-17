62b79f20ec35783b260dde9c9c44a2abc5de53d1
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-04-16 22:12:55 -0300

    Add files via upload

---
+++ userbot/modules/untitled.py
@@ -0,0 +1,25 @@
+from asyncio import create_subprocess_exec as asyncrunapp
+
+from asyncio.subprocess import PIPE as asyncPIPE
+
+from os import remove
+from plataform import python_version, uname
+from shutil import which
+from telethon import version
+
+from userbot import ALIVE_LOGO, ALIVE_NAME, CMD_HELP, LBOT_VERSION, bot
+from userbot.events import register
+
+# ================= CONSTANT ===================
+DEFAULTUSER = ALIVE_NAME or "Defina a ConfigVar 'ALIVE_NAME' !"
+# ===================================≠=≠========
+
+@register(outgoing=True, pattern=r"^\.of$")
+async def amireallyalive(alive):
+  """ Para o comando .alive, verifique se o bot está rodando. """
+  output = (
+    f""" **LzinBot funcionando perfeitamente**
+  • python: v{python_version()}
+  • user: {DEFAULTUSER}
+"""
+  )
