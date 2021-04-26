c48f1dcf38368f9186e684d9a5889d4957e48a44
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-04-25 22:54:19 -0300

    update afk.py

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -89,7 +89,7 @@ async def mention_afk(mention):
     afk_end = {}
     start_1 = datetime.now()
     afk_start = start_1.replace(microsecond=0)
-    afk_time = afk_start
+    total_afk_time = (afk_end - afk_start)
     if mention.message.mentioned and ISAFK:
         is_bot = False
         if (sender := await mention.get_sender()) :
